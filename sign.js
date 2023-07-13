1b3cf5c9d012fc8da4a87964b0c0883d0466677c
amanavinash <119783660+amanavinash@users.noreply.github.com>
2023-07-13 11:42:05 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,25 @@
+let from = document.getElementById("signUp").addEventListener("submit", SignUp);
+
+async function SignUp(e) {
+  e.preventDefault();
+  let name = e.target.name.value;
+  let email = e.target.email.value;
+  let password = e.target.password.value;
+  let phonenumber = e.target.phonenumber.value;
+  let obj = {
+    name,
+    email,
+    password,
+    phonenumber
+  };
+  try {
+    const res = await axios.post("http://localhost:4000/user/sign", obj);
+    
+    alert("User Signed Up");
+    window.location.reload();
+    console.log(res);
+  } catch (error) {
+    alert("Failed");
+    console.log("error:", error);
+  }
+}
